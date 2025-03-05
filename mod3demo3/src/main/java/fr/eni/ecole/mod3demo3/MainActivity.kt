ca0a3d272832049480b6dc03b559439faf216282
Baptiste KARA <baptiste.kara2024@campus-eni.fr>
2025-03-05 10:01:39 +0100

    demo3

--- mod3demo3/src/main/java/fr/eni/ecole/mod3demo3/MainActivity.kt
+++ mod3demo3/src/main/java/fr/eni/ecole/mod3demo3/MainActivity.kt
@@ -1,48 +1,56 @@
-package fr.eni.ecole.mod3demo2
+package fr.eni.ecole.mod3demo3
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import fr.eni.ecole.mod3demo2.ui.theme.CoursAndroidTheme
+import fr.eni.ecole.mod3demo3.ui.theme.CoursAndroidTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Hello(name = "Batou")
+
+
         }
     }
 }
 
 @Composable
-fun Hello(name: String) {
-    Scaffold {
-        Column(modifier = Modifier.padding(it)) {
-            Text(
-                text = "Coucou $name",
-                //color = MaterialTheme.colorScheme.onPrimary,
-                color = Color.Green
-            )
-            Text(text = "Bonjour $name")
+fun RowAndColumnExample() {
+
+    Column{
+        Text("Row and Column example")
+        Row{
+            Text("Row 1, ")
+            Text("Row 2, ")
+            Text("Row 3, ")
+        }
+        Column{
+            Text("Column 1")
+            Text("Column 2")
+            Text("Column 3")
         }
     }
 }
 
-
 @Composable
+fun BoxExample() {
+    Box
+}
+
 @Preview
+@Composable
 fun Preview() {
-    Hello(name = "Batou")
+    RowAndColumnExample()
 }
