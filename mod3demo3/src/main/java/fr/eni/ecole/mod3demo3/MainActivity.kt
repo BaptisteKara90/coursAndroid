2108e57eec6c3bf86ab5eed344a328bc3194d52d
Baptiste KARA <baptiste.kara2024@campus-eni.fr>
2025-03-05 10:13:10 +0100

    mod3demo3

--- mod3demo3/src/main/java/fr/eni/ecole/mod3demo3/MainActivity.kt
+++ mod3demo3/src/main/java/fr/eni/ecole/mod3demo3/MainActivity.kt
@@ -4,14 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import fr.eni.ecole.mod3demo3.ui.theme.CoursAndroidTheme
 
@@ -46,11 +56,24 @@ fun RowAndColumnExample() {
 
 @Composable
 fun BoxExample() {
-    Box
+    Box(
+        contentAlignment = Alignment.BottomCenter,
+        modifier = Modifier.clip(CircleShape)
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.goatkiss),
+            contentDescription = "A goat kiss",
+        )
+        Icon(
+            imageVector = Icons.Default.Favorite,
+            contentDescription = "little heart",
+            tint = Color.Red
+        )
+    }
 }
 
 @Preview
 @Composable
 fun Preview() {
-    RowAndColumnExample()
+    BoxExample()
 }
